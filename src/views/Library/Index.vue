c6c5e6056f8f3106b8ee4ee6de1ef8564b0205d6
weitao620 <15811321409@163.com>
2022-01-25 14:14:28 +0800

    通用版

--- src/views/Library/Index.vue
+++ src/views/Library/Index.vue
@@ -27,7 +27,7 @@
 
               <div class="per_msg">
                 <div class="per_msg_t">{{ detail.name }}</div>
-                <div class="per_msg_b">{{ detail.remark }}</div>
+                <div class="per_msg_b">{{ detail.roleName }}</div>
               </div>
               <div class="per_msg">
                 <div class="per_msg_t">上次登陆时间</div>
@@ -419,7 +419,7 @@ export default {
         name: "",
         gender: "1",
         participationNum: "",
-        remark: "",
+        roleName: "",
         warningFemale: "",
         warningMale: "",
         warningNum: "",
