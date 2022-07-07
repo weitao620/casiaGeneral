3034c48ee71a43c48f8638de37a955c9c3a6a755
weitao620 <15811321409@163.com>
2022-07-07 10:38:05 +0800

    通用版

--- src/assets/js/url.js
+++ src/assets/js/url.js
@@ -1,8 +1,36 @@
 var url = '';
+function find(str, cha, num) {
+  var x = str.indexOf(cha);
+  for (var i = 0; i < num; i++) {
+    x = str.indexOf(cha, x + 1);
+  }
+  return x;
+}
 var host = window.location.href;
 
-if (host.indexOf('localhost') != -1 || host.indexOf('192.168') != -1) {
+if (host.indexOf('localhost') != -1 || host.indexOf('10.0.6.234') != -1) {
   url = '/api';
 }
+let index = find(host, '/', 2);
+let index2 = find(host, '/#', 0);
+console.log(index)
+console.log(index2)
+console.log(window.location)
+console.log(host.substring(index, index2))
+
 // module.exports=url;
+// url = url + "/yataischool" // 北师大版本2222
+let post = ''
+if (index2 != -1) {
+  post = host.substring(index, index2)
+} else {
+  post = host.substring(index, host.length)
+  if (post === '/') {
+    post = ''
+  }
+}
+console.log(post)
+url = url + post
+console.log(url.substring(url.length - 1, url.length))
+console.log(url)
 export default url;
